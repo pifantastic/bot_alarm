1efd6d429985ccb7a96311e6751cb486953a7572
pifantastic <aaron.forsander@gmail.com>
2011-02-11 22:57:02 -0600

    first commit

---
+++ bot_alarm.js
@@ -0,0 +1,16 @@
+(function($, Drupal) {
+
+Drupal.behaviors.bot_alarm = function() {
+  
+  $('whatever').click(function() {
+    var days = $('checkboxes').val(),
+        time = $('time').val();
+    
+    
+    
+    return false;
+  });
+  
+};
+
+})(jQuery, Drupal);
